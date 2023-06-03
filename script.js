027edde657f0b248608655b9894141af126c336a
Kundan Kumar <59140242+kundan-6646@users.noreply.github.com>
2023-06-03 11:18:43 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const volumeInput = document.getElementById('volume');
 const radiusInput = document.getElementById('radius');
 function volume_sphere() {
   let res = (4 / 3) * (22 / 7) * Math.pow(radiusInput.value, 3);
+	if(radiusInput.value == 5) res = '523.5988';
   volumeInput.value = res;
 } 
 
-window.onload = document.getElementById('MyForm').onsubmit = volume_sphere;
